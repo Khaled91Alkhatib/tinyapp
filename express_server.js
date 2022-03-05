7d6bda83ac8ca000b041c7efbf4c032630b979fd
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-03-05 16:40:25 -0500

    Route, after deletion, using POST request

--- express_server.js
+++ express_server.js
@@ -47,6 +47,12 @@ app.get("/u/:shortURL", (req, res) => {
   }
 });
 
+app.post("/urls/:shortURL/delete", (req, res) => {
+  const shortURL = req.params.shortURL;
+  delete urlDatabase[shortURL];
+  res.redirect("/urls");
+});
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
