f5fdf6810d83aa65e5c4c89ec2e7529cd0bb8b2d
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-03-07 13:05:43 -0500

    GET /register endpoint to return registration template

--- express_server.js
+++ express_server.js
@@ -74,6 +74,11 @@ app.post("/logout", (req, res) => {
   res.redirect("/urls");
 });
 
+app.get("/register", (req, res) => {
+  const templateVars = {username: req.cookies["username"]};
+  res.render("urls_registration", templateVars);
+});
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
