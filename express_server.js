380d66ade719298a798c564ec28446c75f06c368
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-03-12 12:31:39 -0500

    Require helper functions and cleanup of code

--- express_server.js
+++ express_server.js
@@ -1,20 +1,18 @@
+const {authenticateUser, generateRandomString, urlsForUser, findUserByShortURL} = require("./helpers");
 const bodyParser = require("body-parser"); // body-parser makes data sent as a buffer to be readable
 const cookieSession = require("cookie-session");
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const app = express();
 const PORT = 8080; // default port 8080
+app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
-
 app.use(cookieSession({
   name: 'session',
   keys: ["anything_randomstring"],
   maxAge: 24 * 60 * 60 * 1000 // represents when will the cookie expire (24 hours here)
-}))
-
-app.set("view engine", "ejs");
-
+}));
 
 // Global variables
 const urlDatabase = {
@@ -39,72 +37,6 @@ const users = {
     email: "user2@example.com",
     password: "dishwasher-funk"
   }
-};
-
-
-// Global functions
-const generateRandomString = function() {
-  let newString = Math.random().toString(36).substring(2, 8);  // the 36 represents base 36; includes all letters and numbers 0123456789
-  return newString;
-};
-
-const findUserByEmail = function(email, users) {  // helper function used in authenticateUser function
-  for (const userID in users) {
-    if (users[userID].email === email) {
-      return true;
-    }
-  }
-  return false;
-};
-
-const findUserByPassword = function(password, users) {  // helper function used in authenticateUser function
-  for (const userID in users) {
-    if (bcrypt.compareSync(password, users[userID].password)) {
-      return true;
-    }
-  }
-  return false;
-};
-
-const authenticateUser = function(email, password, users) {  // helper function used in POST /login and /register
-  for (const user in users) {
-    // console.log(users[user].password, "login database password")
-    // console.log(password, "typed password")
-    if (users[user].email === email && bcrypt.compareSync(password, users[user].password)) {
-      return users[user]; // this is for the POST login
-    }
-  }
-  const userFound = findUserByEmail(email, users);
-  const passwordFound = findUserByPassword(password, users);
-  if (!userFound && !passwordFound ) {
-    return false;
-  }
-  if (userFound || passwordFound) {
-    return true;
-  }
-  return true;
-};
-
-const urlsForUser = function(id, uDb) { // uDb represents the database with the data already in it
-  const newObj = {};
-  for (const k in uDb) {
-    if (id === uDb[k].userID) {
-      newObj[k] = {
-        longURL: uDb[k].longURL,
-        userID: uDb[k].userID
-      };
-    }
-  }
-  return newObj;
-};
-
-const findUserByShortUrl = function(id, uDb) {
-  for (const k in uDb) {
-    if (id === uDb[k].userID) {
-      return true;
-    }
-  }
-  return false;
 };
 
 // GET requests
@@ -135,8 +67,9 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {  // the ":" indicates that shortURL is a route parameter
   const user_id = req.session.user_id;
   const user = users[user_id];
-  const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], userKey: user};
-  if (findUserByShortUrl(user_id, urlDatabase)) {
+  const shortURL = req.params.shortURL
+  const templateVars = {shortURL, longURL: urlDatabase[req.params.shortURL], userKey: user};
+  if (findUserByShortURL (shortURL, user_id, urlDatabase)) {
     res.render("urls_show", templateVars);
   } else {
     res.send("Access to others' URL denied!");
@@ -146,7 +79,7 @@ app.get("/urls/:shortURL", (req, res) => {  // the ":" indicates that shortURL i
 app.get("/u/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   const urlObject = urlDatabase[shortURL];
-  if (!urlDatabase[shortURL]) {  // check for proper/existent shortURL (edge case)
+  if (!urlDatabase[shortURL]) {  // check for proper/existent shortURL
     res.send("Invalid input!");
   } else {
     res.redirect(urlObject.longURL);
@@ -172,7 +105,7 @@ app.get("/login", (req, res) => {
 app.post("/urls", (req, res) => {  // add a post route to receive the form submissions
   const user_id = req.session.user_id;
   const newShorturl = generateRandomString();
-  urlDatabase[newShorturl] = {  // assign new key and key value
+  urlDatabase[newShorturl] = {  // assign new key and key values
     longURL: req.body.longURL,
     userID: user_id
   };
@@ -184,8 +117,9 @@ app.post("/urls", (req, res) => {  // add a post route to receive the form submi
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {
+  const shortURL = req.params.shortURL;
   const user_id = req.session.user_id;
-  if (findUserByShortUrl(user_id, urlDatabase)) {
+  if (findUserByShortURL (shortURL, user_id, urlDatabase)) {
     const shortURL = req.params.shortURL;
     delete urlDatabase[shortURL];
     res.redirect("/urls");
@@ -195,8 +129,9 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 });
 
 app.post("/urls/:id", (req, res) => {  // modifies URL
+  const id = req.params.id;
   const user_id = req.session.user_id;
-  if (findUserByShortUrl(user_id, urlDatabase)) {
+  if (findUserByShortURL (id, user_id, urlDatabase)) {
     const newLongURL = req.body.newLongURL;
     const id = req.params.id;
     urlDatabase[id].longURL = newLongURL;
@@ -207,8 +142,6 @@ app.post("/urls/:id", (req, res) => {  // modifies URL
 app.post("/register", (req, res) => {
   const {email, password} = req.body;
   const hashedPassword = bcrypt.hashSync(password, 10);
-  // console.log(hashedPassword, "hashed")
-
   if (email === "" || password === "") {
     return res.status(400).send("Invalid Credentials");
   }
@@ -222,8 +155,6 @@ app.post("/register", (req, res) => {
     email,
     password: hashedPassword
   };
-  // console.log(users)
-  // res.cookie("user_id", newUserID);
   req.session.user_id = newUserID;
   res.redirect("/urls");
 });
@@ -234,7 +165,6 @@ app.post("/login", (req, res) => {
 
   let user = authenticateUser(email, password, users);
   if (user.email) {
-    // res.cookie("user_id", user.id);
     req.session.user_id = user.id;
     res.redirect("/urls");
   } else {
@@ -246,21 +176,6 @@ app.post("/logout", (req, res) => {
   req.session = null;
   res.redirect("/urls");
 });
-
-
-// Initial GET requests used when initiating tinyapp
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
 
 // Listener
 app.listen(PORT, () => {
